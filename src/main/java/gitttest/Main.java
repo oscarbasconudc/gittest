cc24ae216ffc12e6a501494326588a99cea93227
oscarbasconudc <oscar.bascon@udc.es>
2023-10-25 13:54:36 +0200

    primer commit

---
+++ src/main/java/gitttest/Main.java
@@ -0,0 +1,12 @@
+package gitttest;
+
+public class Main {
+	private static int suma1(int x) {
+		return x + 1;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("holaaa");
+	}
+}
