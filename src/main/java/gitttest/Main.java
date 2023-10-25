8ef7a19f131c8f2be597c8787a0ec8cf6bc8b4d1
oscarbasconudc <oscar.bascon@udc.es>
2023-10-25 14:00:43 +0200

    suma3 y multi

--- src/main/java/gitttest/Main.java
+++ src/main/java/gitttest/Main.java
@@ -1,10 +1,14 @@
 package gitttest;
 
 public class Main {
-	private static int resta1(int x) {
-		return x + 1;
+	private static int multiplica2(int x) {
+		return x * 2;
 	}
 	
+	
+	private static int suma3(int x) {
+		return x + 3;
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("holaaa");
